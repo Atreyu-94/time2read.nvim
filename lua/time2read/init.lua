53293e37e98ef60b2ba8cad375f34fbf7b3a4e9e
Atreyu-94 <39834909+Atreyu-94@users.noreply.github.com>
2023-07-19 18:28:25 -0600

    Update init.lua
    First usable version

--- lua/time2read/init.lua
+++ lua/time2read/init.lua
@@ -1,57 +1,50 @@
 local M = {}
 
-local filetypes = {}
+local filetypes = { "markdown" }
 local wpm = 150
+local preferred_time = 0
 
-local wordcount = vim.fn.wordcount().words
 local visual_wordcount = vim.fn.wordcount().visual_words
 
 local function minutes2read()
-	local result = wordcount / wpm
-	return result
+  local wordcount = vim.fn.wordcount().words
+  local result = wordcount / wpm
+  return result
 end
 
 local function minutes2time(total_minutes)
-	local days = math.floor(total_minutes / 1440)
-	local hours = math.floor((total_minutes - (days * 1440)) / 60)
-	local minutes = math.floor(total_minutes - (days * 1440) - (hours * 60))
+  -- local days = math.floor(total_minutes / 1440)
+  local hours = math.floor((total_minutes % 1440) / 60)
+  local minutes = math.floor((total_minutes % 1440) % 60)
+  local seconds = math.floor((total_minutes * 60) % 60)
 
-	--[[	if days == 0 then
-		days = "00"
-	elseif hours == 0 then
-		hours = "00"
-	elseif minutes == 0 then
-		minutes = "00"
-	end]]
-	--
-
-	return days, hours, minutes
-end
-
-local function filetype_check()
-	return vim.tbl.contains(filetypes, vim.bo.filetype) == true or false
-end
-
-function M.setup(options)
-	options = options or {}
-	filetypes = options.filetypes or { "markdown" }
-	wpm = options.wpm or 150
+  return string.format("%02d:%02d:%02d", hours, minutes, seconds)
 end
 
 function M.time2read_insert()
-	if filetype_check() == true then
-		vim.api.nvim_create_autocmd("InsertCharPre", {
-			callback = minutes2time(minutes2read()),
-		})
+  local filetype_check = vim.tbl_contains(filetypes, vim.bo.filetype)
 
-		return minutes2time(minutes2read).days
-			.. ":"
-			.. minutes2time(minutes2read).hours
-			.. ":"
-			.. minutes2time(minutes2read).minutes
-	else
-		return nil
-	end
+  if filetype_check == false then
+    return nil
+  elseif filetype_check == true then -- and preferred_time == 0 then
+    return minutes2time(minutes2read())
+    -- elseif filetype_check == true and preferred_time ~= 0 then
+    --local percent = tostring(minutes2read() * 100 / preferred_time) .. "%" .. tostring(minutes2time(minutes2read()))
+    --    return percent
+  end
+end
+
+function M.setup(options)
+  options = options or {}
+  filetypes = options.filetypes or { "markdown" }
+  wpm = options.wpm or 150
+  preferred_time = options.preferred_time or 0
+
+  vim.api.nvim_create_autocmd({ "InsertCharPre" }, {
+    -- pattern = { "*.md" },
+    callback = M.time2read_insert,
+    -- group = "time2reader",
+  })
 end
 
 return M
