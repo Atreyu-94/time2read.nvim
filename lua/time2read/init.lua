afe42c6b7d67d21bf43e88f74180641fe6d19c68
atreyu-94 <vlamaro123@gmail.com>
2023-07-20 16:10:42 -0600

    Added option to adjust the desired reading time and get the reading percentage based on that time

--- lua/time2read/init.lua
+++ lua/time2read/init.lua
@@ -4,36 +4,6 @@ local filetypes = { "markdown" }
 local wpm = 150
 local preferred_time = 0
 
-local visual_wordcount = vim.fn.wordcount().visual_words
-
-local function minutes2read()
-	local wordcount = vim.fn.wordcount().words
-	local result = wordcount / wpm
-	return result
-end
-
-local function minutes2time(total_minutes)
-	-- local days = math.floor(total_minutes / 1440)
-	local hours = math.floor((total_minutes % 1440) / 60)
-	local minutes = math.floor((total_minutes % 1440) % 60)
-	local seconds = math.floor((total_minutes * 60) % 60)
-
-	return string.format("%02d:%02d:%02d", hours, minutes, seconds)
-end
-
-function M.time2read_insert()
-	local filetype_check = vim.tbl_contains(filetypes, vim.bo.filetype)
-
-	if filetype_check == false then
-		return nil
-	elseif filetype_check == true and preferred_time == 0 then
-		return minutes2time(minutes2read())
-	elseif filetype_check == true and preferred_time ~= 0 then
-		local percent = tostring(minutes2read() * 100 / preferred_time) .. "%" .. tostring(minutes2time(minutes2read()))
-		return percent
-	end
-end
-
 function M.setup(options)
 	options = options or {}
 	filetypes = options.filetypes or { "markdown" }
@@ -45,6 +15,38 @@ function M.setup(options)
 		callback = M.time2read_insert,
 		-- group = "time2reader",
 	})
+end
+
+local function minutes2read()
+	local wordcount = vim.fn.wordcount().words
+	local result = wordcount / wpm
+	return result
+end
+
+local function minutes2time(total_minutes)
+	local days = math.floor(total_minutes / 1440)
+	local hours = math.floor((total_minutes % 1440) / 60)
+	local minutes = math.floor((total_minutes % 1440) % 60)
+	local seconds = math.floor((total_minutes * 60) % 60)
+
+	return days, hours, minutes, seconds
+end
+
+function M.time2read_insert()
+	local filetype_check = vim.tbl_contains(filetypes, vim.bo.filetype)
+	local _, hours, minutes, seconds = minutes2time(minutes2read())
+
+	if filetype_check == false then
+		return nil
+	elseif filetype_check == true and preferred_time == 0 then
+		return string.format("%02d:%02d′%02d″", hours, minutes, seconds)
+	elseif filetype_check == true and preferred_time ~= 0 then
+		local percent = math.floor(minutes2read() * 100 / preferred_time)
+		local time = string.format("%02d′%02d″", minutes, seconds)
+		local result = tostring(percent) .. "%% " .. time
+
+		return result
+	end
 end
 
 return M
